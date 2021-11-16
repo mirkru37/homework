9c7fd7717153b54a4050fb684fa107854703a7e3
Roman <mirkru37@gmail.com>
2021-11-16 20:23:41 +0200

    Add secret.json

--- Programming/RESTful_api/init.py
+++ Programming/RESTful_api/init.py
@@ -1,23 +1,29 @@
+import json
+
 from flask import Flask
 from flask_mysqldb import MySQL
 from flask_swagger_ui import get_swaggerui_blueprint
 
+SECRET_PATH = "secret.json"
+
 app = Flask(__name__)
 
-SWAGGER_URL = '/api/v1.0/index'
-API_URL = '/static/swagger.json'
-SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
+secret = json.load(open(SECRET_PATH))
+
+SWAGGER_URL = secret["swagger_url"]
+SWAGGER_JSON = secret["swagger_json"]
+SWAGGER_BLUEPRINT = get_swaggerui_blueprint(
     SWAGGER_URL,
-    API_URL,
+    SWAGGER_JSON,
     config={
-        'app_name': "Freelancer-api"
+        'app_name': secret["app_name"]
     }
 )
 
-app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = ''
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'UniversityDB'
+app.register_blueprint(SWAGGER_BLUEPRINT, url_prefix=SWAGGER_URL)
+app.config['MYSQL_HOST'] = secret['host']
+app.config['MYSQL_USER'] = secret['user']
+app.config['MYSQL_PASSWORD'] = secret['password']
+app.config['MYSQL_DB'] = secret['DB']
 
 mysql = MySQL(app)
